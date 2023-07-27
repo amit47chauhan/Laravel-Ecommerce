a82fd1b6739d7a8e841f515ff6f87db258a8d60b
amit47chauhan <chauhanamit.ak47@gmail.com>
2023-07-27 14:34:35 +0530

    added auth page layout

---
+++ resources/views/example-auth.blade.php
@@ -0,0 +1,7 @@
+@extends('back.layout.auth-layout')
+
+@section('pageTitle', isset($pageTitle)? $pageTitle : 'Page Title here')
+
+@section('content')
+    content here.
+@endsection
---
+++ resources/views/example-page.blade.php
@@ -0,0 +1,7 @@
+@extends('back.layout.page-layout')
+
+@section('pageTitle', isset($pageTitle)? $pageTitle : 'Page Title here')
+
+@section('content')
+    content here.
+@endsection
